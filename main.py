3683c38ab7577cab843148cfee58b88c2e4fd360
Soumya Mukhija <soumyamukhija@gmail.com>
2020-07-01 00:51:47 +0530

    datasets introduced and routed

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import os
 from flask import Flask, send_from_directory
 from flask_ngrok import run_with_ngrok
+from progress_data import progress_data
 
 app = Flask(__name__)
 run_with_ngrok(app)
 
 
 @app.route('/')
-def send_dummy_data():
-    data = {"Dummy": "data"}
+def send_welcome_data():
+    data = {"Welcome! Do you want to go abroad to study?": ["Yes", "No", "Maybe"],
+            "What is stopping you from studying abroad?": ["Finances", "Low scores", "Other"]}
     return data
+
+
+@app.route('/progress/', methods=['POST', 'GET'])
+def send_progress_data():
+    prog_data = progress_data()
+    return prog_data
+
+
+@app.route('/univeristy_predictor/', methods={'POST', 'GET'})
+def send_uni_data():
+    return 'Sorry, this is still under construction!'
 
 
 @app.route('/favicon.ico')
---
+++ progress_data.py
@@ -0,0 +1,48 @@
+def progress_data():
+    prog_data = {1: {
+        'Name': 'Know your Universities',
+        'Description': 'To start off, it would be a good idea to read about the course you want to take, the Universities that offer that and make a list of the Universities meeting your needs.',
+        'Related': ['https://www.careeraddict.com/best-courses', 'https://www.timeshighereducation.com/world-university-rankings/by-subject', 'https://globalshala.com/globalshala_blog/if-i-havent-heard-of-a-school-it-cant-be-good-2/']
+    },
+        2: {
+        'Name': 'Prepare for your tests',
+            'Description': 'It would be a good idea to prepare for at least one month for each test that you would need to get in your dream University. Find information about the tests you need to take on your dream University\'s website',
+            'Related': ['https://medium.com/@soumyamukhija/how-i-scored-325-on-my-gre-da42a7081082', 'https://globalshala.com/globalshala_blog/importance-of-sat-and-act/']
+    },
+        3: {
+        'Name': 'Take your tests',
+            'Description': 'Take your tests well before the applications start so you have time to retake them if you need to.',
+            'Related': ['https://www.greedge.com/gre-exam-dates', 'https://globalshala.com/globalshala_blog/remember-things-without-having-to-struggle/']
+    },
+        4: {
+        'Name': 'Write your Statement of Purpose',
+            'Description': 'Write a clean, descriptive essay on your motivation for being a part of your course and what you can contribute to it in the future. Highlight your best qualities and give examples where they helped you with your field.',
+            'Related': ['https://globalshala.com/globalshala_blog/must-have-traits-to-become-successful-entrepreneur/', 'https://globalshala.com/globalshala_blog/remember-things-without-having-to-struggle/']
+    },
+        5: {
+        'Name': 'Request for Letters of Recommendation',
+            'Description': 'Go to 3 different teachers or supervisors who you think know you well and can recommend you for the course you want to undertake. Ask them to write LORs for you. Give them plenty of time to work on your LORs.',
+            'Related': ['https://www.hotcoursesabroad.com/india/prepare-to-apply/how-to-get-a-letter-of-recommendation/3-best-persons-to-ask-for-a-lor/', 'https://studyabroad.shiksha.com/lor-recommendation-letter-applycontent2702#:~:text=Irrespective%20of%20course%20(UG%2C%20MS,professors%2C%20school%20counsellors%20or%20principal.']
+    },
+        6: {
+        'Name': 'Collect your documents',
+            'Description': 'Make a folder with your Resume, SOP, LORs, and all the other documents required for your application. The information regarding required documents is listed on University websites.',
+            'Related': ['https://www.mastersportal.com/articles/2589/what-documents-do-you-need-to-apply-for-a-university-abroad.html', 'http://www.msinus.com/content/documents-required-apply-us-universities-192/']
+    },
+        7: {
+        'Name': 'Send in your applications',
+            'Description': 'Do not be nervous! Send in your applications to 1 dream, 2 reach, and 2 safe Universities (minimum).',
+            'Related': ['https://www.timeshighereducation.com/student/advice/seven-tips-choosing-right-university-you-and-where-find-them', 'https://www.topuniversities.com/blog/how-choose-university-6-tips']
+    },
+        8: {
+        'Name': 'Send in your applications',
+            'Description': 'Do not be nervous! Send in your applications to 1 dream, 2 reach, and 2 safe Universities (minimum).',
+            'Related': ['https://www.timeshighereducation.com/student/advice/seven-tips-choosing-right-university-you-and-where-find-them', 'https://www.topuniversities.com/blog/how-choose-university-6-tips']
+    },
+        9: {
+        'Name': 'Apply for scholarships',
+            'Description': 'Financing Universities can be very hard. Fortunately, Globalshala is there to help you with it! Read related articles to know about scholarships.',
+            'Related': ['https://globalshala.com/global_event/scholarship-interview/']
+    }
+    }
+    return prog_data
