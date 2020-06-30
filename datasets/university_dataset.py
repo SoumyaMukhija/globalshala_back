6dfbeced4ef33c37e460e751425181b674ef83de
Soumya Mukhija <soumyamukhija@gmail.com>
2020-07-01 00:18:57 +0530

    datasets updated

--- datasets/university_dataset.py
+++ datasets/university_dataset.py
@@ -1,3 +1,7 @@
+'''This dataset belongs to: 
+Mohan S Acharya, Asfia Armaan, Aneeta S Antony : A Comparison of Regression Models for Prediction of Graduate Admissions, IEEE International Conference on Computational Intelligence in Data Science 2019'''
+
+
 import pandas as pd
 import requests
 
