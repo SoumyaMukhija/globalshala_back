1ed5fc9a98975cb6168ab3b7d261af9e28c1199c
sumit mukhija <sumitmukhija@hotmail.com>
2020-06-30 22:40:07 +0100

    Remove favicon & base endpoint. Change dictionary structure progress_data

--- main.py
+++ main.py
@@ -6,17 +6,9 @@ from progress_data import progress_data
 app = Flask(__name__)
 run_with_ngrok(app)
 
-
-@app.route('/')
-def send_welcome_data():
-    data = {"Welcome! Do you want to go abroad to study?": ["Yes", "No", "Maybe"],
-            "What is stopping you from studying abroad?": ["Finances", "Low scores", "Other"]}
-    return data
-
-
 @app.route('/progress/', methods=['POST', 'GET'])
 def send_progress_data():
-    prog_data = progress_data()
+    prog_data = {"progress": progress_data()}
     return prog_data
 
 
@@ -24,7 +16,5 @@ def send_progress_data():
 def send_uni_data():
     return 'Sorry, this is still under construction!'
 
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/img/'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+if __name__ == '__main__':
+    app.run()
