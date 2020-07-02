42f480877bd7a30266b748052d1258ef9aaf94e5
Soumya Mukhija <soumyamukhija@gmail.com>
2020-07-02 20:30:28 +0530

    feed data added and routed

--- main.py
+++ main.py
@@ -2,20 +2,12 @@ import os
 from flask import Flask, send_from_directory
 from flask_ngrok import run_with_ngrok
 from progress_data import progress_data
+from feed_data import feed_data
 
 app = Flask(__name__)
 run_with_ngrok(app)
 
-<<<<<<< HEAD
-@app.route('/')
-def send_welcome_data():
-    data = {"Welcome! Do you want to go abroad to study?": ["Yes", "No", "Maybe"],
-            "What is stopping you from studying abroad?": ["Finances", "Low scores", "Other"]}
-    return data
 
-
-=======
->>>>>>> 0b37de59013556871225fc8442d099d07b443bc7
 @app.route('/progress/', methods=['POST', 'GET'])
 def send_progress_data():
     prog_data = {"progress": progress_data()}
@@ -25,6 +17,13 @@ def send_progress_data():
 @app.route('/univeristy_predictor/', methods={'POST', 'GET'})
 def send_uni_data():
     return 'Sorry, this is still under construction!'
+
+
+@app.route('/feed_data/', methods{'POST', 'GET'})
+def send_feed_data():
+    fd = {"feed data": feed_data()}
+    return fd
+
 
 if __name__ == '__main__':
     app.run()
